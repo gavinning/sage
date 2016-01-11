7916675a8c86ab41f7ecddf71d2fde1196fa2e5b
gavinning <ningyubo@gmail.com>
2016-01-11 16:11:10 +0800

    add quickstart

--- sage.js
+++ sage.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 var path = require('path');
+var showdown  = require('showdown');
+var Markdown = new showdown.Converter();
 var router = system.express.Router();
 var User = system.db.User;
 
@@ -10,4 +12,9 @@ exports.static = path.join(__dirname, 'static');
 
 router.get('/', function(req, res){
     res.status(200).send('Hello ' + exports.name)
+})
+
+router.get('/quickstart', function(req, res){
+    var md = Markdown.makeHtml(fs.readFileSync(path.join(__dirname, 'views/quickstart.md'), 'utf-8'));
+    res.render(path.join(__dirname, 'views/md'), {md: md})
 })
---
+++ views/quickstart.md
@@ -0,0 +1,15 @@
+# 快速开始
+***
+
+### Installs
+* [Install Nodejs](http://nodejs.org/)
+* [Install UZ](https://www.npmjs.com/package/uz)
+* [Install Aimee-cli](https://www.npmjs.com/package/aimee-cli)
+
+### Create demo
+```
+$: aimee create pro
+$: cd pro
+$: uz release -p
+$: uz server start
+```
